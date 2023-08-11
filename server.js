d974281c11e95f68aabd7eafe7dc22fe6a793afa
umairaghaa <umair.agha@griffithuni.edu.au>
2023-08-11 10:49:29 +1000

    updated server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+
+app.use(express.static(__dirname + '/www'));
+
+let server = http.listen(3000,function(){
+    let host = server.address().address;
+    let port = server.address().port;
+    console.log("Server listening on: " + host + "port: " +port);
+});
