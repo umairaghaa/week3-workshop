72c7c0e41d05452363f70573767f3931e78ce645
umairaghaa <umair.agha@griffithuni.edu.au>
2023-08-11 11:00:35 +1000

    jqery installed, test.html added

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var http = require('http').Server(app);
 
 app.use(express.static(__dirname + '/www'));
 
-let server = http.listen(3000,function(){
+let server = http.listen(3001,function(){
     let host = server.address().address;
     let port = server.address().port;
-    console.log("Server listening on: " + host + "port: " +port);
+    console.log("Server listening on: "+ host + "port: " +port);
 });
